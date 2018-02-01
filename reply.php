97596c94dd490504d22305e485a5bb14080526ff
asifkhan123 <35867614+asifkhan123@users.noreply.github.com>
2018-02-01 15:45:03 +0800

    Update reply.php

--- reply.php
+++ reply.php
@@ -1,4 +1,8 @@
 <html>
+  <head>
+    <title>PHP</title>
+  </head>
+<body>
 <?php echo("<html><body bgcolor=\"#a38692\">");?>
 <?php echo("<h1><center> Your survey has been completed</center></h1>");?>
 You are a <?php echo $_POST["Gender"]; ?>!<br />
@@ -6,4 +10,5 @@ Your favorite singer is <?php echo $_POST["Teacher"]; ?> class. <br />
 You like him as a singer because he is <?php echo $_POST["reason"]; ?> class. <br />
 The type of music you prefer listening <?php echo $_POST["genre"]; ?> class. <br />
 <?php echo("</html></body>");?>
+  </body>
 </html>
