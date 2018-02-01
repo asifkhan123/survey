feb7f96f9e481c75fe977dfe4eeb14813abae7ec
asifkhan123 <35867614+asifkhan123@users.noreply.github.com>
2018-02-01 15:38:40 +0800

    Create reply.php

---
+++ reply.php
@@ -0,0 +1,7 @@
+<?php echo("<html><body bgcolor=\"#a38692\">");?>
+<?php echo("<h1><center> Your survey has been completed</center></h1>");?>
+You are a <?php echo $_POST["Gender"]; ?>!<br />
+Your favorite singer is <?php echo $_POST["Teacher"]; ?> class. <br />
+You like him as a singer because he is <?php echo $_POST["reason"]; ?> class. <br />
+The type of music you prefer listening <?php echo $_POST["genre"]; ?> class. <br />
+<?php echo("</html></body>");?>
